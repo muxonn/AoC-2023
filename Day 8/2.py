a9fb559f9419a8a13b988307d8b502ac388f8a92
Jakub Madry <jaiwiecejmnie@gmail.com>
2023-12-09 17:17:45 +0100

    Add Day 8 part 2

--- Day 8/2.py
+++ Day 8/2.py
@@ -1,3 +1,5 @@
+import math
+
 with open("data.txt") as f:
     file = f.read().splitlines()
 
@@ -8,34 +10,33 @@ for i in file[2:]:
     element = i.split()
     elements.update({element[0]: [element[2][1:4], element[3][:3]]})
 
-last_nodes = []
+start_nodes = []
 for i in elements:
     if i[-1] == 'A':
-        last_nodes.append(i)
-print(last_nodes)
+        start_nodes.append(i)
+print(start_nodes)
 
-def check_end(items):
-    for i in items:
-        if i[-1] != "Z":
-            return False
-    return True
-
-steps = 0
-found = False
-
-while found is False:
-    for i in instructions:
-        for n, item in enumerate(last_nodes):
+all_steps = []
+for item in start_nodes:
+    last = item
+    steps = 0
+    found = False
+    while found is False:
+        for i in instructions:
             if i == 'L':
-                last_nodes[n] = elements[item][0]
+                last = elements[last][0]
             elif i == 'R':
-                last_nodes[n] = elements[item][1]
+                last = elements[last][1]
+            steps += 1
+            if last[-1] == 'Z':
+                all_steps.append(steps)
+                found = True
+                break
 
-        steps += 1
-        if check_end(last_nodes):
-            found = True
-            break
-            
-        if steps % 100000 == 0:
-            print(steps)
-print(steps)
+def find_lcm_of_list(numbers):
+    lcm = numbers[0]
+    for num in numbers[1:]:
+        lcm = math.lcm(lcm, num)
+    return lcm
+
+print(find_lcm_of_list(all_steps))
---
+++ Day 8/tempCodeRunnerFile.py
@@ -0,0 +1,15 @@
+for item in start_nodes:
+#     last = item
+#     steps = 0
+#     found = False
+#     while found is False:
+#         for i in instructions:
+#             if i == 'L':
+#                 last = elements[last][0]
+#             elif i == 'R':
+#                 last = elements[last][1]
+#             steps += 1
+#             if last == 'ZZZ':
+#                 found = True
+#                 break
+# print(steps)
